be6b0950a669051af8b81b6e92750c9e89c28796
chandans1999 <85291105+chandans1999@users.noreply.github.com>
2021-06-03 22:14:25 +0530

    BillingRepository

---
+++ src/main/java/com/capg/ebs/repository/BillingRepository.java
@@ -0,0 +1,11 @@
+package com.capg.ebs.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.capg.ebs.entities.Entitybill;
+
+public interface BillerRepository extends JpaRepository<EntityBill,Long>{
+	
+	
+
+}
