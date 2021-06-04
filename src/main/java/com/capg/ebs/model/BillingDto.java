cb2c93b96e3d459c7aaf3e0a0351faacb5632f0b
chandans1999 <85291105+chandans1999@users.noreply.github.com>
2021-06-04 18:21:15 +0530

    BillingDto

--- src/main/java/com/capg/ebs/model/BillingDto.java
+++ src/main/java/com/capg/ebs/model/BillingDto.java
@@ -1,52 +1,44 @@
 package com.capg.ebs.model;
+import java.time.LocalDate;
 
 
-
-
-import org.springframework.stereotype.Component;
-
-
-
-@Component
 
 public class BillingDto {
 
-	private int billId;
-	private int billNum;
-	private  double grandTotal;
-	private double totalTaxAmount;
-	private int quantity;
-	public int getBillId() {
-		return billId;
-	}
-	public void setBillId(int billId) {
-		this.billId = billId;
-	}
-	public int getBillnum() {
-		return billNum;
-	}
-	public void setBillnum(int billnum) {
-		this.billNum = billnum;
-	}
-	public double getGrandTotal() {
-		return grandTotal;
-	}
-	public void setGrandTotal(double grandTotal) {
-		this.grandTotal = grandTotal;
-	}
-	public double getTotalTaxAmount() {
-		return totalTaxAmount;
-	}
-	public void setTotalTaxAmount(double totalTaxAmount) {
-		this.totalTaxAmount = totalTaxAmount;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
+private int billId;
+private int billNum;
+private  double grandTotal;
+private LocalDate date;
+public int getBillId() {
+	return billId;
+}
+public void setBillId(int billId) {
+	this.billId = billId;
+}
+public int getBillnum() {
+	return billNum;
+}
+public void setBillnum(int billnum) {
+	this.billNum = billnum;
+}
+public double getGrandTotal() {
+	return grandTotal;
+}
+public void setGrandTotal(double grandTotal) {
+	this.grandTotal = grandTotal;
+}
+
+public LocalDate getDate() {
+	return date;
+}
+public void setDate(LocalDate date) {
+	this.date = date;
+}
+@Override
+public String toString() {
+	return "Billing [billId=" + billId + ", billNum=" + billNum + ", grandTotal=" + grandTotal + ", date=" + date + "]";
+}
+
+
 	
-	}
-
-
 }
