5170fb1c70b0481331f625609b9bb721ba487fcd
javva <javva@LAPTOP-TK14A734>
2021-06-04 08:19:33 +0530

    payment page added

---
+++ src/main/java/com/capg/ebs/entities/Payment.java
@@ -0,0 +1,38 @@
+package com.capg.ebs.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+	@Table(name="Payment")
+	public class Payment {
+		
+
+		@Id
+		private long creditcardNo;
+		private long debitcardNo;
+		private long netbanking;
+		public long getCreditcardNo() {
+			return creditcardNo;
+		}
+		public void setCreditcardNo(long creditcardNo) {
+			this.creditcardNo = creditcardNo;
+		}
+		public long getDebitcardNo() {
+			return debitcardNo;
+		}
+		public void setDebitcardNo(long debitcardNo) {
+			this.debitcardNo = debitcardNo;
+		}
+		public long getNetbanking() {
+			return netbanking;
+		}
+		public void setNetbanking(long netbanking) {
+			this.netbanking = netbanking;
+		}
+		
+		
+
+	}
+
