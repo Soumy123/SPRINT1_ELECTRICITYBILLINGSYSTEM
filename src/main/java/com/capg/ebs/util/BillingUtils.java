6945302420885d0a434be069ea8ed127cc32d5f8
Soumya <Soumya@DESKTOP-EDGFEOD>
2021-06-04 17:32:11 +0530

    BillingUtil

---
+++ src/main/java/com/capg/ebs/util/BillingUtils.java
@@ -0,0 +1,8 @@
+package com.capg.ebs.util;
+
+public class BillingUtils {
+
+//	public static List<ComplaintDetailsDTO> convertToComplaintDetailsDTOList(List<ComplaintDetails> list){
+	//	List<ComplaintDetailsDTO> dtolist = new ArrayList<ComplaintDetailsDTO>();
+
+}
