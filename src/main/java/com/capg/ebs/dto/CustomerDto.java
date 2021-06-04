1e0d6c0641ce9dfef3fa814bb4d7b76387fc830d
Soumya <Soumya@DESKTOP-EDGFEOD>
2021-06-04 10:14:35 +0530

    serviceimp

--- src/main/java/com/capg/ebs/dto/CustomerDto.java
+++ src/main/java/com/capg/ebs/dto/CustomerDto.java
@@ -6,18 +6,7 @@ public class CustomerDto {
 
 	private String customerName;
 	private int customerId;
-	public String getCustomerName() {
-		return customerName;
-	}
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
-	public int getCustomerId() {
-		return customerId;
-	}
-	public void setCustomerId(int customerId) {
-		this.customerId = customerId;
-	}
+	
 	
 	
 
---
+++ src/main/java/com/capg/ebs/services/BillingServiceImpl.java
@@ -0,0 +1,37 @@
+package com.capg.ebs.services;
+
+import java.util.List;
+
+import com.capg.ebs.entities.Billing;
+
+public class BillingServiceImpl implements IBillingService {
+
+	@Override
+	public Billing addBills(Billing bill) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteBills(Billing bill) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Billing updateBills(Billing billName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Billing> getAllBills() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+	
+
+}
