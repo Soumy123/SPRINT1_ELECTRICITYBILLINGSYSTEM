f5c7725753a658df570d66b9c516cea5706883eb
Soumya <Soumya@DESKTOP-EDGFEOD>
2021-06-04 17:24:25 +0530

    Billingutil

--- src/main/java/com/capg/ebs/util/BillingUtils.java
+++ src/main/java/com/capg/ebs/util/BillingUtils.java
@@ -1,5 +1,19 @@
 package com.capg.ebs.util;
 
-public class BillingUtils {
+import java.util.List;
 
-}
+import com.capg.ebs.dto.BillingDto;
+
+public class BillingUtils {
+	
+	//public List<BillingDto> convertToBillingDTOList(List<BillingDto> list) {
+		
+		
+		//dtolist.add(convertToBillingDTO(Billing));
+		
+		//return dtolist;
+	}
+	
+
+	
+
