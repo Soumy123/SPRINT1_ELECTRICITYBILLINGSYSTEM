d0f08e863d0029f9ae69ddf5ff7ef4aa60f15199
chandans1999 <85291105+chandans1999@users.noreply.github.com>
2021-06-05 17:05:19 +0530

    IBillingService

--- src/main/java/com/capg/ebs/services/IBillingService.java
+++ src/main/java/com/capg/ebs/services/IBillingService.java
@@ -1,11 +1,13 @@
 package com.capg.ebs.services;
 import java.util.List;
+
+import com.capg.ebs.dto.BillingDto;
 import com.capg.ebs.entities.Billing;
 
 public interface IBillingService {
 	
-	public Billing addBills(Billing bill);
+	public BillingDto addBills(Billing bill);
         public void deleteBills(Billing bill);
-        public Billing updateBills(Billing billName);
-	public List<Billing> getAllBills();
+        public BillingDto updateBills(Billing billName);
+	public List<BillingDto> getAllBills();
 }
