303b3f11ef8bf48e8b1422f7ec62902ba15c0800
Soumya <Soumya@DESKTOP-EDGFEOD>
2021-06-05 19:25:17 +0530

    merge

--- src/main/java/com/capg/ebs/dto/BillingDto.java
+++ src/main/java/com/capg/ebs/dto/BillingDto.java
@@ -1,4 +1,4 @@
-package com.capg.ebs.model;
+package com.capg.ebs.dto;
 
 
 
--- src/main/java/com/capg/ebs/model/CustomerDto.java
+++
@@ -1,9 +0,0 @@
-package com.capg.ebs.model;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class CustomerDto {
-	
-
-}
