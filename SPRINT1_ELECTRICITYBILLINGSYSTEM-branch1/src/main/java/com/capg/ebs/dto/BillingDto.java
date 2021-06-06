4d69f6406f61d029646b6dc3cdcdd41ef34c741d
chandans1999 <85291105+chandans1999@users.noreply.github.com>
2021-06-06 20:54:46 +0530

    billing

--- SPRINT1_ELECTRICITYBILLINGSYSTEM-branch1/src/main/java/com/capg/ebs/dto/BillingDto.java
+++ SPRINT1_ELECTRICITYBILLINGSYSTEM-branch1/src/main/java/com/capg/ebs/dto/BillingDto.java
@@ -1,79 +1,76 @@
 package com.capg.ebs.dto;
 import java.time.LocalDate;
 
-import org.springframework.stereotype.Component;
+import javax.persistence.CascadeType;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
 
 import com.capg.ebs.entities.Customer;
-import com.capg.ebs.entities.Units;
 
 
 
-
-@Component
 
 public class BillingDto {
 
-	private int billId;
-	private Customer customer;
-	private int billNum;
-	private int units;
-	private  double grandTotal=Units.calculateBillPay(units);
+private Long billId;
+@OneToOne(cascade=CascadeType.ALL)
+@JoinColumn(name="customerId")
+private Customer customer;
+private int billNum;
+private int units;
 
-	public int getBillNum() {
-		return billNum;
-	}
-	public void setBillNum(int billNum) {
-		this.billNum = billNum;
-	}
-	public int getUnits() {
-		return units;
-	}
-	public void setUnits(int units) {
-		this.units = units;
-	}
-	private LocalDate date;
+private LocalDate date;
+private  double grandTotal=UnitsDto.calculateBillPay(units);
 
-	private double totalTaxAmount;
+public int getBillNum() {
+	return billNum;
+}
+public void setBillNum(int billNum) {
+	this.billNum = billNum;
+}
+public int getUnits() {
+	return units;
+}
+public void setUnits(int units) {
+	this.units = units;
+}
 
-	public int getBillId() {
-		return billId;
-	}
-	public void setBillId(int billId) {
-		this.billId = billId;
-	}
-	public int getBillnum() {
-		return billNum;
-	}
-	public void setBillnum(int billnum) {
-		this.billNum = billnum;
-	}
-	public double getGrandTotal() {
-		return grandTotal;
-	}
-	public void setGrandTotal(double grandTotal) {
-		this.grandTotal = grandTotal;
-	}
 
-	public LocalDate getDate() {
-		return date;
-	}
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+public Long getBillId() {
+	return billId;
+}
+public void setBillId(Long billId) {
+	this.billId = billId;
+}
+public int getBillnum() {
+	return billNum;
+}
+public void setBillnum(int billnum) {
+	this.billNum = billnum;
+}
 
-	public Customer getCustomer() {
-		return customer;
-	}
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
 
-	@Override
-	public String toString() {
-		return "Billing [billId=" + billId + ", billNum=" + billNum + ", units=" + units + ", grandTotal=" + grandTotal
-				+ ", date=" + date + "]";
+public LocalDate getDate() {
+	return date;
+}
+public void setDate(LocalDate date) {
+	this.date = date;
+}
 
-	}
+public Customer getCustomer() {
+	return customer;
+}
+public void setCustomer(Customer customer) {
+	this.customer = customer;
+}
 
+@Override
+public String toString() {
+	return "Billing [billId=" + billId + ", billNum=" + billNum + ", units=" + units + ", grandTotal=" + grandTotal
+			+ ", date=" + date + "]";
+
+}
 }
 
