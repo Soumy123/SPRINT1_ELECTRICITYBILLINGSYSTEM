5f43ecc71318859273149ee26fd899956e1b55ad
Soumy123 <85154888+Soumy123@users.noreply.github.com>
2021-06-08 09:27:44 +0530

    Update BillingDto.java

--- SPRINT1_ELECTRICITYBILLINGSYSTEM-branch1/src/main/java/com/capg/ebs/dto/BillingDto.java
+++ SPRINT1_ELECTRICITYBILLINGSYSTEM-branch1/src/main/java/com/capg/ebs/dto/BillingDto.java
@@ -12,17 +12,24 @@ import com.capg.ebs.entities.Customer;
 
 
 
-public class BillingDto {
+@Entity
+@Table(name="billing")
 
+public class BillingDTO {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+@NotNull(message="Bill Id cannot be null")
 private Long billId;
 @OneToOne(cascade=CascadeType.ALL)
 @JoinColumn(name="customerId")
 private Customer customer;
+@NotEmpty(message="Bill Num cannot be Empty")
 private int billNum;
-private int units;
+@Min(value=10,message="should be greater than 10")
+private static int units;
 
 private LocalDate date;
-private  double grandTotal=UnitsDto.calculateBillPay(units);
+private  double grandTotal=calculateBillPay(units);
 
 public int getBillNum() {
 	return billNum;
@@ -30,7 +37,7 @@ public int getBillNum() {
 public void setBillNum(int billNum) {
 	this.billNum = billNum;
 }
-public int getUnits() {
+public static int getUnits() {
 	return units;
 }
 public void setUnits(int units) {
@@ -65,12 +72,28 @@ public Customer getCustomer() {
 public void setCustomer(Customer customer) {
 	this.customer = customer;
 }
+
+public static double calculateBillPay(int units) {
+	   
+    units=getUnits();
+	   double billpay=0;
+	
+    if(units<100)
+	 billpay= units*1.20;
+
+	    else if(units<300 &&units>100)
+	billpay=100*1.20+(units-100)*2;
+
+ else if(units>300)
+	billpay=100*1.20+200 *2+(units-300)*3;
+return billpay;
+    }
+
 @Override
 public String toString() {
-	return "BillingDto [billId=" + billId + ", customer=" + customer + ", billNum=" + billNum + ", units=" + units
-			+ ", date=" + date + ", grandTotal=" + grandTotal + "]";
+	return "Billing [billId=" + billId + ", billNum=" + billNum + ", units=" + units + ", grandTotal=" + grandTotal
+			+ ", date=" + date + "]";
+
 }
-
-
 }
 
